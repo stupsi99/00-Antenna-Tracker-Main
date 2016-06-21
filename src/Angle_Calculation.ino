fa7d5ff92aeaea8e6db1bb970b9d66a7a379e676
stupsi99 <Phil@Philipps-MacBook-Pro-2.fritz.box>
2016-06-21 10:21:59 +0200

    Variables moved from main to angle calculation

--- src/Angle_Calculation.ino
+++ src/Angle_Calculation.ino
@@ -1,6 +1,39 @@
 #include <Arduino.h>
 
 int i_quadrant = 0; //to determine quadrant number
+float f_currentAzimut = 0; //the actual azimuth angle the tracker is pointing at currently
+float f_currentElevation = 0;
+
+//flying object coordinates
+float f_object_lat = 48.170639;
+float f_object_lon = 11.570251;
+float f_object_alt = 15;
+
+//pointing angles and flying object information
+float f_azimut = 0; //[°]
+float f_elevation = 0; //[°]
+float f_altitude_to_ground = 0; //[m]
+float f_distance_ground = 0; //[m]
+float f_total_distance = 0; //calculation not implemented (line of sight distance to object)
+
+//helping variables
+int positionValid = 0; //only if GPS could be decoded successfully, the angle calculation starts
+byte gpsString;
+int i_gps_counter = 0;
+unsigned long time_last_decode = 0, time_current_decode = 0;
+
+//test home coordinates (middle Luitpoldpark)
+float f_home_lat = 48.168926;
+float f_home_lon = 11.568848;
+float f_home_altitude = 0; //[m]
+
+/*Test Luitpold Park
+Home Positon: 48.168926,11.568848
+Quadrant 1: 48.170639,11.570251, Azimut: 28,61°, 217,32m Distance on ground;
+Quadrant 2: 48.167487,11.570463, Azimut: 143,19°, 199,97m Distance on ground;
+Quadrant 3: 48.167821,11.566685, Azimut: 232,52°, 202,98m Distance on ground
+Quadrant 4: 48.170246,11.567995, Azimut: 336,67°, 160,00m Distance on ground
+*/
 
 void angleCalculation() {
   float f_delta_lat = f_object_lat - f_home_lat;
@@ -109,12 +142,13 @@ void angleCalculation() {
 
 
 
-
-    //Serial1.print("Total distance: ");
-    //Serial1.println(f_distance_ground);
-    //Serial1.print("Altitude to ground: ");
-    //Serial1.println(f_altitude_to_ground);
-    //Serial1.print("Elevation: ");
-    //Serial1.println(f_elevation);
+    /*
+    Serial.print("Total distance: ");
+    Serial.println(f_distance_ground);
+    Serial.print("Altitude to ground: ");
+    Serial.println(f_altitude_to_ground);
+    Serial.print("Azimut: ");
+    Serial.println(f_azimut);
+    */
 
 }
